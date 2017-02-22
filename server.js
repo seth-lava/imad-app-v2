50b48b68ef90fea26d8d64a847e4d1ec1d718751
seth-lava <lavanshu.seth@gmail.com>
2017-02-22 13:02:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/article', function(res){
     //res.send('Article is to be printed here');
-    res.sendFile(path.join(__diename, 'ui','article.html'));
+    res.sendFile(path.join(__dirname, 'ui','article.html'));
 });
 
 app.get('/magic' , function(req,res){
